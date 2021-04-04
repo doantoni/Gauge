9520892a024e14b2ef010d1892eca7ff1f0e216c
doantoni <65489913+doantoni@users.noreply.github.com>
2021-04-04 18:56:26 +0200

    Update carMeter.js

--- carMeter.js
+++ carMeter.js
@@ -10,70 +10,70 @@ let rotateIncrement = 45;
 
 // Indicators for left circle
 
-for(let i=1; i<=40; i++){
+    for(let i=1; i<=40; i++){
+        
+        //create 40 circle divs and append to left
     
-    //create 40 circle divs and append to left
- 
-     divs[i] = document.createElement("div");
-     divs[i].className = "circle";
-     leftCircle.append(divs[i]);
+        divs[i] = document.createElement("div");
+        divs[i].className = "circle";
+        leftCircle.append(divs[i]);
+        
+        // create indicators and long indicators
+        const indicator = document.createElement("div");
+        indicator.className = "indicator";
+        const indicatorLong = document.createElement("div");
+        indicatorLong.className = "indicator-long";
+        
+        // append indicators to their rightful locations
     
-     // create indicators and long indicators
-     const indicator = document.createElement("div");
-     indicator.className = "indicator";
-     const indicatorLong = document.createElement("div");
-     indicatorLong.className = "indicator-long";
-     
-     // append indicators to their rightful locations
- 
-     if(i % 4 !== 0){
-         divs[i].append(indicator);
-     } else {
-         divs[i].append(indicatorLong);
-     }
-     divs[i].style.transform = `rotate(${i*9}deg)`;
- }
+        if(i % 4 !== 0){
+            divs[i].append(indicator);
+        } else {
+            divs[i].append(indicatorLong);
+        }
+        divs[i].style.transform = `rotate(${i*9}deg)`;
+    }
 
     
  
 
  // Indicators for right circle
 
-for(let i=1; i<=40; i++){
+    for(let i=1; i<=40; i++){
+        
+        //create 40 circle divs and append to right
     
-    //create 40 circle divs and append to right
- 
-     divs[i] = document.createElement("div");
-     divs[i].className = "circle";
-     rightCircle.append(divs[i]);
+        divs[i] = document.createElement("div");
+        divs[i].className = "circle";
+        rightCircle.append(divs[i]);
+        
+        // create indicators and long indicators
+        const indicator = document.createElement("div");
+        indicator.className = "indicator";
+        const indicatorLong = document.createElement("div");
+        indicatorLong.className = "indicator-long";
+        
+        // append indicators to their rightful locations
     
-     // create indicators and long indicators
-     const indicator = document.createElement("div");
-     indicator.className = "indicator";
-     const indicatorLong = document.createElement("div");
-     indicatorLong.className = "indicator-long";
-     
-     // append indicators to their rightful locations
- 
-     if(i % 4 !== 0){
-         divs[i].append(indicator);
-     } else {
-         divs[i].append(indicatorLong);
-     }
-     divs[i].style.transform = `rotate(${i*9}deg)`;
- }
+        if(i % 4 !== 0){
+            divs[i].append(indicator);
+        } else {
+            divs[i].append(indicatorLong);
+        }
+        divs[i].style.transform = `rotate(${i*9}deg)`;
+    }
 
 // Creating array of long indicators
 
- const allLongIndicators = document.querySelectorAll(".circle .indicator-long");
- console.log(allLongIndicators);
+        const allLongIndicators = document.querySelectorAll(".circle .indicator-long");
+        console.log(allLongIndicators);
 
 // Creating paragraph elements inside long indicators
 
- for (let i = 0; i<allLongIndicators.length ; i++){
-    const innerParagraphs = document.createElement("p");
-    allLongIndicators[i].appendChild(innerParagraphs);
- }
+        for (let i = 0; i<allLongIndicators.length ; i++){
+            const innerParagraphs = document.createElement("p");
+            allLongIndicators[i].appendChild(innerParagraphs);
+        }
 
  // Selecting all paragraphs inside long indicators
 
@@ -141,6 +141,7 @@ for(let i=1; i<=40; i++){
  paragraphs[9].style.zIndex = "-1";
 
 
+// Gas pedal logic
 
 
 gasPedal.addEventListener("click", function(){
@@ -150,11 +151,14 @@ gasPedal.addEventListener("click", function(){
     }     
 )
 
+// Brake pedal logic
+
 brakePedal.addEventListener("click", function(){
     leftArrow[0].style.transform = "rotate(0deg)";
     leftArrow[1].style.transform = "rotate(0deg)" 
 })
 
+//Shift pedal logic
 
 shiftGear.addEventListener("click", function (){
     rotateIncrement += 45;
